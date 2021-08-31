8d364d5e4efb11d1420d52f24798dc5ce9a0da0f
tobi michael <tobimichael65@gmail.com>
2021-08-31 16:24:24 +0100

    new new

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"os"
 )
+type User struct {
+	Name string  `json:"name"`
+	Age int `json:"age"`
+	Email string `json:"email"`
+	BloodType string `json:"blood_type"`
+}
 
 func main() {
 	// create a new gin router
@@ -17,7 +23,65 @@ func main() {
 		port = "3000"
 	}
 	_ = router.Run(":" + port)
+
+	// CRUD endpoints for data
+
+	router.POST("/createUser", createUserHandler)
+	router.GET("/getUser", getSingleUserHandler)
+	router.GET("/getUsers", getAllUserHandler)
+	router.PATCH("/updateUser", updateUserHandler)
+	router.DELETE("/deleteUser", deleteuserhandler)
+
 }
+
+
+func createUserHandler(c *gin.Context) {
+
+	var user User
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "invalid request data",
+		})
+		return
+
+	}
+	c.JSON(200, gin.H{
+		"message": "successfully created user",
+		"data":    user})
+}
+
+
+func getAllUserHandler(c *gin.Context) {
+			c.JSON(200, gin.H{
+			"message": "hello world",
+			})
+}
+
+func getSingleUserHandler(c *gin.Context)  {
+	var user User
+	user = User{
+		Name: "Big tobss",
+		Age: 5431,
+		Email: "tmichael65@email.com",
+	}
+	c.JSON(200, gin.H{
+		"message": "hello world",
+		"data": user,
+	})
+}
+func updateUserHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "User updated!",
+	})
+}
+func deleteuserhandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "user deleted!",
+	})
+}
+
 
 
 	// run the server on the port 3000
@@ -27,3 +91,4 @@ func helloWorldhandler(c *gin.Context) {
 		"message": "Scooby doo",
 	})
 }
+
